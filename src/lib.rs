c84bd52c5ac2a6b8ec75202f44dcacd48d0bc507
Inanna Malick <inanna@recursion.wtf>
2020-08-10 18:56:25 -0700

    buggy impl of full alg - annoying jitter problem

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub struct State {
     link: ComponentLink<Self>,
     counter: usize,
     interval_task: IntervalTask,
+    clicked: bool,
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -23,12 +24,13 @@ impl Component for State {
     type Properties = ();
 
     fn create(arg: Self::Properties, link: ComponentLink<Self>) -> Self {
-        let one_second = Duration::new(0, 1000);
+        let one_second = Duration::new(0, 10000);
         let interval_task = IntervalService::new().spawn(one_second, link.callback(|()| Msg::Tick));
         State {
             interval_task,
             counter: 0,
             link,
+            clicked: false,
         }
     }
 
@@ -36,38 +38,101 @@ impl Component for State {
         match msg {
             Msg::Tick => {
                 self.counter = (self.counter + 1) % 1000;
-
                 // redraw
                 true
             }
             _ => {
-                // TODO: handle click events
-                false
+                self.clicked = !self.clicked;
+                true
             }
         }
     }
 
     fn view(&self) -> Html {
         let mut iso = Isometric::new();
-        iso.scale3d(3.0, 3.0, 3.0);
+        iso.scale3d(7.0, 7.0, 7.0);
+
+        let t = self.counter as f64 / 100.0;
+        let t = t % 2.0;
+        let t = (-1.0 + t).abs();
+
+        // let cube = mk_cube(&mut iso, t, 0.0, 0.0, 0.0);
+
+        let color = if self.clicked { "#d33682" } else { "#ffffff" };
+
+        let grid_size = 10;
+
+        let scale = 2.0;
+
+        let mut cubes = Vec::new();
+
+        for x in (-grid_size..grid_size) {
+            for y in (-grid_size..grid_size) {
+                // why is this here
+                let d = distance_manhattan(x, y);
+                if (d < 8) {
+
+                    let x = x as f64;
+                    let y = y as f64;
+
+                    let te = cubic_in_out(0.0_f64.max(1.0_f64.min(t * 4.8 - distance_cartesian(x, y) / 5.0)));
+
+                    //if (Math.abs(x) >= innergridsize || Math.abs(y) >= innergridsize) continue;
+                    //elems += 1;
+
+                    // I think this is a faithful translation? lmao idk
+                    let dd = if d & 1 > 0 { 1.0 } else { -1.0 };
+
+                    let cube = mk_cube(
+                        &mut iso,
+                        dd * (FRAC_PI_4 - te * FRAC_PI_2),
+                        x * scale * 1.42,
+                        y * scale * 1.42,
+                        te * 0.5,
+                    );
+
+                    cubes.push((cube, format!("{:.2}", te)));
+                } else {
+                }
+            }
+        }
 
 
-        let t = self.counter as f64 / 1000.0;
-        let t = t * PI;
-
-        let cube = mkCube(&mut iso, t, 0.0, 0.0, 0.0);
-        let outer_path = path_to_points(cube.outer_path);
-        html! {
-            <svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
-            <polygon points=outer_path
-            fill="none" stroke="black" />
+        html!{
+            <div>
+            <svg viewBox="-100 -50 200 100" xmlns="http://www.w3.org/2000/svg">
+            { for cubes.into_iter().rev().map(|(cube, t)| {
+                // let t = "foo";
+                self.render_cube(cube, color.to_string(), t.to_string())
+            })
+            }
             </svg>
+            <text>{ format!("t = {:.3}", t) } </text>
+            </div>
         }
     }
 }
 
+impl State {
+    fn render_cube(&self, cube: Cube, color: String, text: String) -> Html {
+        let outer_path = path_to_points(cube.outer_path);
+        let inner_path = path_to_points(cube.inner_path);
 
-struct Point{
+        html!{
+        <g>
+            <polygon points=outer_path fill=color stroke="black" stroke-width="0.5"
+            onclick=self.link.callback( |_| Msg::Click )
+            />
+            <polyline points=inner_path fill="none" stroke="black" stroke-width="0.25"
+             onclick=self.link.callback( |_| Msg::Click )
+            />
+            <text x=cube.text_anchor.x y=cube.text_anchor.y font-size="2" text-anchor="middle">{ text }</text>
+        </g>
+        }
+    }
+}
+
+struct Point {
     x: f64,
     y: f64,
 }
@@ -75,31 +140,31 @@ struct Point{
 fn path_to_points(path: Vec<Point>) -> String {
     let mut s = String::from("");
     for point in path {
-        s.push_str(&format!("{:.2},{:.2} ", point.x + 10.0, point.y + 10.0));
+        s.push_str(&format!("{:.2},{:.2} ", point.x, point.y));
     }
     s
 }
 
-
 struct Cube {
     inner_path: Vec<Point>,
     outer_path: Vec<Point>,
+    text_anchor: Point,
 }
 
-fn mkCube(iso: &mut Isometric, angle: f64, x: f64, y: f64, z: f64) -> Cube {
+fn mk_cube(iso: &mut Isometric, angle: f64, x: f64, y: f64, z: f64) -> Cube {
     let mut angle = angle % FRAC_PI_2;
-    if (angle  < 0.0) {
+    if (angle < 0.0) {
         angle += FRAC_PI_2;
     };
     iso.save();
     iso.translate3d(x, y, z);
-    iso.rotateZ(angle - FRAC_PI_4);
+    iso.rotate_z(angle - FRAC_PI_4);
 
     //context.fillStyle = fill;
     //context.globalAlpha = alpha;
 
-    let cubeDim = 0.8;
-    let h = cubeDim;
+    let cube_dim = 0.8;
+    let h = cube_dim;
 
     //11.39,9.20
     //12.94,5.90
@@ -110,12 +175,12 @@ fn mkCube(iso: &mut Isometric, angle: f64, x: f64, y: f64, z: f64) -> Cube {
 
     let mut outer_path = Vec::new();
 
-    outer_path.push(iso.transform(cubeDim, -cubeDim, cubeDim));
-    outer_path.push(iso.transform(cubeDim, cubeDim, cubeDim));
-    outer_path.push(iso.transform(-cubeDim, cubeDim, cubeDim));
-    outer_path.push(iso.transform(-cubeDim, cubeDim, -cubeDim));
-    outer_path.push(iso.transform(-cubeDim, -cubeDim, -cubeDim));
-    outer_path.push(iso.transform(cubeDim, -cubeDim, -cubeDim));
+    outer_path.push(iso.transform(cube_dim, -cube_dim, cube_dim));
+    outer_path.push(iso.transform(cube_dim, cube_dim, cube_dim));
+    outer_path.push(iso.transform(-cube_dim, cube_dim, cube_dim));
+    outer_path.push(iso.transform(-cube_dim, cube_dim, -cube_dim));
+    outer_path.push(iso.transform(-cube_dim, -cube_dim, -cube_dim));
+    outer_path.push(iso.transform(cube_dim, -cube_dim, -cube_dim));
 
     //iso.closePath();
     //context.fill();
@@ -124,12 +189,12 @@ fn mkCube(iso: &mut Isometric, angle: f64, x: f64, y: f64, z: f64) -> Cube {
 
     //context.beginPath();
     let mut inner_path = Vec::new();
-    inner_path.push(iso.transform(-cubeDim, -cubeDim, cubeDim));
-    inner_path.push(iso.transform(cubeDim, -cubeDim, cubeDim));
-    inner_path.push(iso.transform(-cubeDim, -cubeDim, cubeDim));
-    inner_path.push(iso.transform(-cubeDim, cubeDim, cubeDim));
-    inner_path.push(iso.transform(-cubeDim, -cubeDim, cubeDim));
-    inner_path.push(iso.transform(-cubeDim, -cubeDim, -cubeDim));
+    inner_path.push(iso.transform(-cube_dim, -cube_dim, cube_dim));
+    inner_path.push(iso.transform(cube_dim, -cube_dim, cube_dim));
+    inner_path.push(iso.transform(-cube_dim, -cube_dim, cube_dim));
+    inner_path.push(iso.transform(-cube_dim, cube_dim, cube_dim));
+    inner_path.push(iso.transform(-cube_dim, -cube_dim, cube_dim));
+    inner_path.push(iso.transform(-cube_dim, -cube_dim, -cube_dim));
     //context.lineWidth = 0.75;
     //context.stroke();
 
@@ -141,30 +206,35 @@ fn mkCube(iso: &mut Isometric, angle: f64, x: f64, y: f64, z: f64) -> Cube {
     //   context.fillStyle = fill;
     // }
 
+    let text_anchor = iso.transform(0.0,0.0,cube_dim);
+
     iso.restore();
 
-    Cube{ inner_path, outer_path}
-  }
+    Cube {
+        inner_path,
+        outer_path,
+        text_anchor,
+    }
+}
 
-
-fn distanceCartesian(x: f64, y: f64) -> f64 {
+fn distance_cartesian(x: f64, y: f64) -> f64 {
     return (x * x + y * y).sqrt();
 }
 
 // distance from (0,0), I think
 // TODO: might be usize?
-fn distanceManhattan(x: isize, y: isize) -> isize {
+fn distance_manhattan(x: isize, y: isize) -> isize {
     x.abs() + y.abs()
 }
 
 // ((t *= 2) <= 1 ? t * t * t : (t -= 2) * t * t + 2) / 2
-fn cubicInOut(t: f64) -> f64 {
+fn cubic_in_out(t: f64) -> f64 {
     let t = t * 2.0;
     let x = if (t <= 1.0) {
         t * t * t
     } else {
-        let t2 = t - 2.0;
-        (t2 * t * t + 2.0) / 2.0
+        let t = t - 2.0;
+        (t * t * t + 2.0) / 2.0
     };
     x
 }
@@ -197,9 +267,7 @@ impl Isometric {
         let x = x3 * self.projection[0] + y3 * self.projection[1];
         let y = x3 * self.projection[2] + y3 * self.projection[3] - z3;
 
-        Point {
-            x, y
-        }
+        Point { x, y }
     }
 
     // seems to take points relative to current iso - so +cube height, -cube width, etc
@@ -244,7 +312,7 @@ impl Isometric {
     // | e f g h | * | sin  cos 0 0 | = | e * cos + f * sin e * -sin + f * cos g h |
     // | i j k l |   |   0    0 1 0 |   | i * cos + j * sin i * -sin + j * cos k l |
     // | 0 0 0 1 |   |   0    0 0 1 |   |                 0                  0 0 1 |
-    fn rotateZ(&mut self, angle: f64) {
+    fn rotate_z(&mut self, angle: f64) {
         let cos = angle.cos();
         let sin = angle.sin();
         let a = self.matrix[0];
@@ -271,105 +339,3 @@ impl Isometric {
         self.matrix[11] += self.matrix[8] * tx + self.matrix[9] * ty + self.matrix[10] * tz;
     }
 }
-
-// constructor(context) {
-//   //this._moveTo = context.moveTo.bind(context);
-//   //this._lineTo = context.lineTo.bind(context);
-//   //this._text = context.fillText.bind(context);
-//   //this.closePath = context.closePath.bind(context);
-//   this._matrix = [
-//     1, 0, 0, 0,
-//     0, 1, 0, 0,
-//     0, 0, 1, 0
-//   ];
-//   this._matrixes = [];
-//     this._projection = [
-//     Math.cos(Math.PI / 6), Math.cos(Math.PI - Math.PI / 6),
-//     -Math.sin(Math.PI / 6), -Math.sin(Math.PI - Math.PI / 6)
-//   ];
-// }
-
-// _project(point, x, y, z) {
-//   point(
-//     x * this._projection[0] + y * this._projection[1],
-//     x * this._projection[2] + y * this._projection[3] - z
-//   );
-// }
-// _transform(point, x, y, z) {
-//   this._project(
-//     point,
-//     x * this._matrix[0] + y * this._matrix[1] + z * this._matrix[2] + this._matrix[3],
-//     x * this._matrix[4] + y * this._matrix[5] + z * this._matrix[6] + this._matrix[7],
-//     x * this._matrix[8] + y * this._matrix[9] + z * this._matrix[10] + this._matrix[11]
-//   );
-// }
-
-// save() {
-//   this._matrixes.push(this._matrix.slice());
-// }
-// restore() {
-//   if (this._matrixes.length) this._matrix = this._matrixes.pop();
-// }
-
-// // | a b c d |   | kx  0  0 0 |   | a * kx b * ky c * kz d |
-// // | e f g h | * |  0 ky  0 0 | = | e * kx f * ky g * kz h |
-// // | i j k l |   |  0  0 kz 0 |   | i * kx j * ky k * kz l |
-// // | 0 0 0 1 |   |  0  0  0 1 |   |      0      0      0 1 |
-// scale3d(kx, ky, kz) {
-//   this._matrix[0] *= kx;
-//   this._matrix[1] *= ky;
-//   this._matrix[2] *= kz;
-//   this._matrix[4] *= kx;
-//   this._matrix[5] *= ky;
-//   this._matrix[6] *= kz;
-//   this._matrix[8] *= kx;
-//   this._matrix[9] *= ky;
-//   this._matrix[10] *= kz;
-// }
-
-// // | a b c d |   | cos -sin 0 0 |   | a * cos + b * sin a * -sin + b * cos c d |
-// // | e f g h | * | sin  cos 0 0 | = | e * cos + f * sin e * -sin + f * cos g h |
-// // | i j k l |   |   0    0 1 0 |   | i * cos + j * sin i * -sin + j * cos k l |
-// // | 0 0 0 1 |   |   0    0 0 1 |   |                 0                  0 0 1 |
-// rotateZ(angle) {
-//   const cos = Math.cos(angle);
-//   const sin = Math.sin(angle);
-//   const a = this._matrix[0];
-//   const b = this._matrix[1];
-//   const e = this._matrix[4];
-//   const f = this._matrix[5];
-//   const i = this._matrix[8];
-//   const j = this._matrix[9];
-//   this._matrix[0] = a * cos + b * sin;
-//   this._matrix[1] = a * -sin + b * cos;
-//   this._matrix[4] = e * cos + f * sin;
-//   this._matrix[5] = e * -sin + f * cos;
-//   this._matrix[8] = i * cos + j * sin;
-//   this._matrix[9] = i * -sin + j * cos;
-// }
-
-// // | a b c d |   | 1 0 0 tx |   | a b c a * tx + b * ty + c * tz + d |
-// // | e f g h | * | 0 1 0 ty | = | e f g e * tx + f * ty + g * tz + h |
-// // | i j k l |   | 0 0 1 tz |   | i j k i * tx + j * ty + k * tz + l |
-// // | 0 0 0 1 |   | 0 0 0  1 |   | 0 0 0                            1 |
-// translate3d(tx, ty, tz) {
-//   this._matrix[3] += this._matrix[0] * tx + this._matrix[1] * ty + this._matrix[2] * tz;
-//   this._matrix[7] += this._matrix[4] * tx + this._matrix[5] * ty + this._matrix[6] * tz;
-//   this._matrix[11] += this._matrix[8] * tx + this._matrix[9] * ty + this._matrix[10] * tz;
-// }
-
-//   text(x, y, z, t) {
-//     var _text = this._text;
-//     var f = function(x,y) {
-//       _text(t,x,y);
-//     };
-//     this._transform(f, x, y, z);
-//   }
-
-//   moveTo(x, y, z) {
-//     this._transform(this._moveTo, x, y, z);
-//   }
-//   lineTo(x, y, z) {
-//     this._transform(this._lineTo, x, y, z);
-//   }
-// }
